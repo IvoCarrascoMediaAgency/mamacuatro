f385374467befabfcf3c35daee4f835395a6cc4f
IvoCarrascoMediaAgency <40969344+IvoCarrascoMediaAgency@users.noreply.github.com>
2018-11-16 16:00:00 -0500

    msmphp1

--- enviar1.php
+++ enviar1.php
@@ -7,5 +7,6 @@
     $mensaje = $_POST["mensaje"];
     $contenido = "Nombre: " . $nombre . "\nCorreo:" . $correo . "\nTelèfono:" . $telefono . "\nMensaje:" . $mensaje;
     mail($destino,"Contacto", $contenido);
+    echo "Gracias Su reserva se está procesando, por favor revise su correo o WhatsApp.";
     header("Location:https://schoolgirlish-therm.000webhostapp.com/#foodies");
     ?>
